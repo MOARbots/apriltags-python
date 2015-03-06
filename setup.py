1bf7c6b95203ea7b8240fa5d0c461f2491ca3ca9
Liam Marshall <archimedespi3141@gmail.com>
2015-03-05 19:28:41 -0600

    Not sure if this is *exactly* right, but it can now be published

--- setup.py
+++ setup.py
@@ -1,2 +1,25 @@
 from distutils.core import setup
 from distutils.extension import Extension
+
+
+setup(
+	name = "apriltags",
+	version = "1.0.dev1",
+	description = "Cython bindings to AprilTags-C",
+	url = "https://github.com/MOARbots/apriltags-python",
+	author = "Liam Marshall",
+	author_email = "archimedespi3141@gmail.com",
+	license = "GPLv3",
+	classifiers = [
+		'Development Status :: 2 - Alpha',
+		'Intended Audience :: Developers',
+		'Intended Audience :: Education',
+		'Topic :: Scientific/Engineering :: Image Recognition',
+		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+		'Programming Language :: Python :: 2.7',
+		'Programming Language :: Cython'
+	],
+	keywords = 'computervision imagerecognition cython bindings apriltags tagging',
+	packages = find_packages(),
+	install_requires = ['cython']
+)
