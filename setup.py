7debbd639219eeb40176365eeff11cb5717176fd
Liam Marshall <archimedespi3141@gmail.com>
2015-03-06 21:24:34 -0600

    For debugging, comment out everything in setup(...) except ext_modules

--- setup.py
+++ setup.py
@@ -6,23 +6,25 @@ from distutils.extension import Extension
 from Cython.Build import cythonize
 
 setup(
-	name = "apriltags",
-	version = "1.0.dev1",
-	description = "Cython bindings to AprilTags-C",
-	url = "https://github.com/MOARbots/apriltags-python",
-	author = "Liam Marshall",
-	author_email = "archimedespi3141@gmail.com",
-	license = "GPLv3",
-	classifiers = [
-		'Development Status :: 2 - Alpha',
-		'Intended Audience :: Developers',
-		'Intended Audience :: Education',
-		'Topic :: Scientific/Engineering :: Image Recognition',
-		'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Cython'
-	],
-	keywords = 'computervision imagerecognition cython bindings apriltags tagging',
-	install_requires = ['cython'],
-	ext_modules = cythonize([Extension("apriltags", [os.path.join('src', 'apriltags.pyx')])])
+	# name = "apriltags",
+	# version = "1.0.dev1",
+	# description = "Cython bindings to AprilTags-C",
+	# url = "https://github.com/MOARbots/apriltags-python",
+	# author = "Liam Marshall",
+	# author_email = "archimedespi3141@gmail.com",
+	# license = "GPLv3",
+	# classifiers = [
+	# 	'Development Status :: 2 - Alpha',
+	# 	'Intended Audience :: Developers',
+	# 	'Intended Audience :: Education',
+	# 	'Topic :: Scientific/Engineering :: Image Recognition',
+	# 	'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+	# 	'Programming Language :: Python :: 2.7',
+	# 	'Programming Language :: Cython'
+	# ],
+	# keywords = 'computervision imagerecognition cython bindings apriltags tagging',
+	# install_requires = ['cython'],
+	ext_modules = cythonize([
+		Extension("apriltags", ['apriltags.pyx'], libraries = ['capriltag'])
+	])
 )
