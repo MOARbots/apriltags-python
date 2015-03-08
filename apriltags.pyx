dfa16fe88a36f2f39364d8b1145804daac1e9a87
Liam Marshall <archimedespi3141@gmail.com>
2015-03-08 09:34:29 -0500

    Change self._apriltag_detector_t -> self._apriltag_detector

--- apriltags.pyx
+++ apriltags.pyx
@@ -2,19 +2,19 @@ cimport capriltag
 
 cdef class AprilTagDetector:
 	# Define a type referencing a struct pointer
-	cdef capriltag.apriltag_detector_t* _apriltag_detector_t
+	cdef capriltag.apriltag_detector_t* _apriltag_detector
 
 	# When the object is initialized by Python
 	def __cinit__(self):
 		# Create a new detector
-		self._apriltag_detector_t = capriltag.apriltag_detector_create()
+		self._apriltag_detector = capriltag.apriltag_detector_create()
 		# If that call went wrong and didn't return a pointer
-		if self._apriltag_detector_t is NULL:
+		if self._apriltag_detector is NULL:
 			raise MemoryError()
 
 	# When Python GCs the object
 	def __dealloc__(self):
 		# As long as the detector succeeded
-		if self._apriltag_detector_t is not NULL:
+		if self._apriltag_detector is not NULL:
 			# Free the struct
-			capriltag.apriltag_detector_destroy(self._apriltag_detector_t)
+			capriltag.apriltag_detector_destroy(self._apriltag_detector)
