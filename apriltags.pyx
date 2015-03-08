c00ca408c26f911a2ad7c5e47bc829d4f9a31c41
Liam Marshall <archimedespi3141@gmail.com>
2015-03-08 18:00:50 -0500

    cdefs for all the tag families

--- apriltags.pyx
+++ apriltags.pyx
@@ -1,7 +1,25 @@
 cimport capriltag
 
+# Giant block of cdefs for all the tag families
+
 cdef extern from "tag16h5.h":
 	capriltag.apriltag_family_t* tag16h5_create();
+
+cdef extern from "tag25h7.h":
+	capriltag.apriltag_family_t* tag25h7_create();
+
+cdef extern from "tag25h9.h":
+	capriltag.apriltag_family_t* tag25h9_create();
+
+cdef extern from "tag36artoolkit.h":
+	capriltag.apriltag_family_t* tag36artoolkit_create();
+
+cdef extern from "tag36h10.h":
+	capriltag.apriltag_family_t* tag36h10_create();
+
+cdef extern from "tag36h11.h":
+	capriltag.apriltag_family_t* tag36h11_create();
+
 
 cdef class AprilTagFamily:
 	cdef capriltag.apriltag_family_t* _apriltag_family
